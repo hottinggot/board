8d7fc8fd436002e0f51c6451bb47a40c7c67ad65
hottinggot <hottinggot@naver.com>
2020-07-10 16:01:38 +0900

    second commit

--- src/main/java/com/example/board/board/controller/BoardController.java
+++ src/main/java/com/example/board/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.example.board.board.dto.BoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.board.board.service.BoardService;
 
@@ -33,6 +34,27 @@ public class BoardController {
     @RequestMapping("/board/insertBoard.do")
     public String insertBoard(BoardDto board) throws Exception{
         boardService.insertBoard(board);
+        return "redirect:/board/openBoardList.do";
+    }
+    @RequestMapping("/board/openBoardDetail.do")
+    public ModelAndView openBoardDetail(@RequestParam("board_idx") int boardIdx) throws Exception{
+        ModelAndView mv = new ModelAndView("/board/boardDetail");
+
+        BoardDto board = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board",board);
+
+        return mv;
+    }
+
+    @RequestMapping("/board/updateBoard.do")
+    public String updateBoard(BoardDto board) throws Exception{
+        boardService.updateBoard(board);
+        return "redirect:/board/openBoardList.do";
+    }
+
+    @RequestMapping("/board/deleteBoard.do")
+    public String deleteBoard(int boardIdx) throws Exception{
+        boardService.deleteBoard(boardIdx);
         return "redirect:/board/openBoardList.do";
     }
 }
